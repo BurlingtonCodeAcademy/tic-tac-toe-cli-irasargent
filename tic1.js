28cc6b1a5db43703910ba8951eb14c53f30259e0
irasargent <46683808+irasargent@users.noreply.github.com>
2019-03-02 08:31:14 -0500

    printed the grid

---
+++ tic1.js
@@ -0,0 +1,16 @@
+let cell1 = [1]
+let cell2 = [2]
+let cell3 = [3]
+let cell4 = [4]
+let cell5 = [5]
+let cell6 = [6]
+let cell7 = [7]
+let cell8 = [8]
+let cell9 = [9]
+
+let divider = '--- --- ---'
+let topRow = ' '+cell1+' | '+cell2+' | '+cell3
+let midRow = ' '+cell4+' | '+cell5+' | '+cell6
+let botRow = ' '+cell7+' | '+cell8+' | '+cell9
+
+console.log('\n'+topRow+'\n'+divider+'\n'+midRow+'\n'+divider+'\n'+botRow)
